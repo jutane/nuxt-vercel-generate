7d975e88bb80904f993ead114d09d3a801a76c4c
Jake Walters <jake@nevado.co.uk>
2023-11-24 13:20:11 +0000

    better describe generated links

--- pages/en/index.vue
+++ pages/en/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <NuxtLink to="/en/product/example/">
-            Example product
+        <NuxtLink to="/en/product/trailing_no_generate/">
+            This document doesn't generate correctly
         </NuxtLink>
         <br>
-        <NuxtLink to="/en/product/test/">
-            Test product
+        <NuxtLink to="/en/product/no_trailing_generates">
+            This document generates correctly
         </NuxtLink>
     </div>
 </template>
